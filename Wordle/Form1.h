23a3eb91309c28507a8f60ebec178fccc733e10c
Yuliya <yuliya18@gmail.com>
2024-12-24 11:11:00 +0300

    Объявление функций и переменных, добавление функций InitializeWords и SelectRandomWord

--- Wordle/Form1.h
+++ Wordle/Form1.h
@@ -1,59 +1,66 @@
-#pragma once
+// 1. Подключение библиотек и объявление пространства имен
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
-namespace Worlde {
+namespace CppCLRWinFormsProject {
 
-	using namespace System;
-	using namespace System::ComponentModel;
-	using namespace System::Collections;
-	using namespace System::Windows::Forms;
-	using namespace System::Data;
-	using namespace System::Drawing;
+    using namespace System;
+    using namespace System::Windows::Forms;
+    using namespace System::Collections::Generic;
 
-	/// <summary>
-	/// Summary for Form1
-	/// </summary>
-	public ref class Form1 : public System::Windows::Forms::Form
-	{
-	public:
-		Form1(void)
-		{
-			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
-		}
+    // 2. Определение класса Form1
+    public ref class Form1 : public System::Windows::Forms::Form
+    {
+    private:
+        static List<String^>^ wordsToGuess; // Список слов для угадывания
+        array<TextBox^>^ textBoxes; // Массив текстовых полей
+        Panel^ menuPanel; // Панель меню
+        Panel^ gamePanel; // Игровая панель
+        Button^ playButton; // Кнопка "Играть"
+        System::Windows::Forms::Label^ label2; // Заголовок игры
+        int attemptCount = 0; // Счётчик попыток
+        String^ correctWord; // Переменная для хранения правильного слова
 
-	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		~Form1()
-		{
-			if (components)
-			{
-				delete components;
-			}
-		}
+    public:
+        // 3. Конструктор
+        Form1(void)
+        {
+            InitializeComponent(); // Инициализация компонентов формы
+            CreateMenu(); // Создание главного меню
+            CreateGamePanel(); // Создание игровой панели
+            SubscribeToTextChangedEvents(); // Подписка на события изменения текста
+            SubscribeToKeyDownEvents(); // Подписка на события нажатия клавиш
+            InitializeWords(); // Инициализация списка слов
+        }
 
-	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		System::ComponentModel::Container ^components;
+    protected:
+        // 4. Деструктор
+        ~Form1()
+        {
+            if (components)
+            {
+                delete components; // Освобождение ресурсов
+            }
+        }
 
-#pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		void InitializeComponent(void)
-		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-		}
-#pragma endregion
-	};
-}
+    private:
+        System::ComponentModel::IContainer^ components; // Контейнер компонентов
+
+        // 5. Инициализация слов
+        void InitializeWords()
+        {
+            wordsToGuess = gcnew List<String^>();
+            wordsToGuess->Add("Актёр");
+            // Добавьте больше слов при необходимости
+            SelectRandomWord(); // Выбор случайного слова для угадывания
+        }
+
+        // 6. Выбор случайного слова
+        void SelectRandomWord()
+        {
+            Random^ rand = gcnew Random();
+            int index = rand->Next(wordsToGuess->Count);
+            correctWord = wordsToGuess[index]; // Установка правильного слова
+        }
